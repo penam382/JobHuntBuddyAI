0832d03ceb03050342a939f0535256f540d6d3a9
Marco Pena <114798389+penam382@users.noreply.github.com>
2025-01-08 12:27:01 -0700

    styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import './Home.css'
+
 function Home() {
     return (
         <>
         <h1>Job Hunt Buddy AI</h1>
         <div className="main-text">
-            <h1>LET'S GET YOUR DREAM JOB.</h1>
+        <h1>LET'S GET YOUR <span className="highlight">DREAM</span> JOB.</h1>
             <p>Your AI Buddy is here to guide, support, and empower you every step of the way.</p>
         </div>
         <button>GET STARTED</button>
